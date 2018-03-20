e76efae0c81d222411febe2491dde99ba03a4780
LourencoNeto <36769786+LourencoNeto@users.noreply.github.com>
2018-03-20 11:33:57 -0300

    Update selection_menu.py

--- selection_menu.py
+++ selection_menu.py
@@ -6,16 +6,18 @@ pygame.init()
 surface_height = 500
 surface_width = 240
 main_surface = pygame.display.set_mode((surface_width, surface_height))
+font = pygame.font.Font(None, 20)
 clock = pygame.time.Clock()
-list = ["Arsenal", "Liverpool", "Chelsea"]
+list = ["Arsenal", "Liverpool", "Chelsea"] #pegar todas os nomes do banco de dados
 box_list = []
-y = 30
+y = 40
 for name in list:
     Box = SelectBox(70, y, 100, name)
     box_list.append(Box)
     y += 20
 Next_Button = Button(130, 430, 100, 40, 34, 13, "Next")
 main_surface.fill((33, 94, 33))
+main_surface.blit(font.render("Selecione seu time:", True, pygame.Color("black")), (10, 10))
 Next_Button.draw(main_surface)
 done = False
 while not done:
